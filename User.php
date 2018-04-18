c88b30d689b1c40c03ae2bb99f47826690b287d9
ryan o <rohouens@yahoo.fr>
2018-04-18 12:22:26 +0200

    adding of getter and setter of all attributes

--- User.php
+++ User.php
@@ -3,13 +3,37 @@ class User extends Raw{
     protected $_pseudo;
     protected $_email;
     protected $_password;
-    protected $_cle;
+    protected $_key;
     protected $_extra;
     protected $_active;
     protected $_creation;
 
     public function getPseudo(){
         return $this->_pseudo;
+    }
+
+    public function getEmail(){
+        return $this->_email;
+    }
+
+    public function getPassword(){
+        return $this->_password;
+    }
+
+    public function getKey(){
+        return $this->_key;
+    }
+
+    public function getExtra(){
+        return $this->_extra;
+    }
+
+    public function getActive(){
+        return $this->_active;
+    }
+
+    public function getCreation(){
+        return $this->_creation;
     }
 
     public function setPseudo($pseudo){
@@ -22,5 +46,29 @@ class User extends Raw{
             return;
         }
         $this->_pseudo = $pseudo;
+    }
+
+    public function setEmail($email){
+         $this->_email = $email;
+    }
+
+    public function setPassword($password){
+         $this->_password = $password;
+    }
+
+    public function setKey($key){
+         $this->_key = $key;
+    }
+
+    public function setExtra($extra){
+         $this->_extra = $extra;
+    }
+
+    public function setActive($active){
+         $this->_active = $active;
+    }
+
+    public function setCreation($creation){
+         $this->_creation = $creation;
     }
 }
