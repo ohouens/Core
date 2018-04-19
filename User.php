54f4e442aa67934541ab9eb022778eb9b0141df9
ryan o <rohouens@yahoo.fr>
2018-04-19 10:50:09 +0200

    upgrade of setters

--- User.php
+++ User.php
@@ -38,37 +38,69 @@ class User extends Raw{
 
     public function setPseudo($pseudo){
         if(!preg_match("#^.{1,20}#$", $pseudo)){
-            trigger_error("Pseudo must be length from 1 to 20 characters" E_USER_WARNING);
+            trigger_error("Pseudo must be length from 1 to 20 characters", E_USER_WARNING);
             return;
         }
         if(!preg_match("#^[a-z0-9_]{1,20}$#"), $pseudo){
-            trigger_error("Pseudo can only contain lower case, underscore and numbers" E_USER_WARNING);
+            trigger_error("Pseudo can only contain lower case, underscore and numbers", E_USER_WARNING);
             return;
         }
         $this->_pseudo = $pseudo;
     }
 
     public function setEmail($email){
-         $this->_email = $email;
+        if(!preg_match("#^[a-z0-9-_.]{2,}@[a-z]+\.[a-z]{2,}$#")){
+            trigger_error("Incorect format of email", E_USER_WARNING);
+            return;
+        }
+        $this->_email = $email;
     }
 
     public function setPassword($password){
-         $this->_password = $password;
+        if(!preg_match("#.{8,}#", $password)){
+            trigger_error("Password must be at least 8 characters", E_USER_WARNING);
+            return;
+        }
+        if(!preg_match("#[a-z]#", $password)){
+            trigger_error("Password must at least contain 1 lower case", E_USER_WARNING);
+            return;
+        }
+        if(!preg_match("#[A-Z]#", $password)){
+            trigger_error("Password must at least contain 1 upper case", E_USER_WARNING);
+            return;
+        }
+        if(!preg_match("#[0-9]#", $password)){
+            trigger_error("Password must at least contain 1 number", E_USER_WARNING);
+            return;
+        }
+        $this->_password = $password;
     }
 
     public function setKey($key){
-         $this->_key = $key;
+        if(!preg_match("#^[a-zA-Z0-9]{32}$#", $key)){
+            trigger_error("Key must contain 32 characters", E_USER_WARNING);
+            return;
+        }
+        $this->_key = $key;
     }
 
     public function setExtra($extra){
-         $this->_extra = $extra;
+        $this->_extra = $extra;
     }
 
     public function setActive($active){
-         $this->_active = $active;
+        if(preg_match("^[0-9]{1}$", $active)){
+            trigger_error("Activation code must be 1 number", E_USER_WARNING);
+            return;
+        }
+        $this->_active = $active;
     }
 
     public function setCreation($creation){
-         $this->_creation = $creation;
+        if(preg_match("^[0-9]{1,}$", $creation)){
+            trigger_error("Date of creation must an integer" E_USER_WARNING);
+            return;
+        }
+        $this->_creation = $creation;
     }
 }
