00b2d9b7663aa52be65fc06e78715af8fd1dce5e
ryan o <rohouens@yahoo.fr>
2018-04-19 22:58:29 +0200

    removal of adding, deleting and updating methods

--- Manager.php
+++ Manager.php
@@ -5,14 +5,8 @@ abstract class Manager{
     public function __construct(PDO $db){
     	$this->_db = $db;
     }
-
-    public abstract function add(Raw $data);
-
-	public abstract function delete(Raw $data);
-
+    
 	public abstract function get($id);
 
 	public abstract function getList();
-
-	public abstract function update(Raw $data);
 }
