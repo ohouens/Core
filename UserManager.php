40441862de603c42924a9898f4a29117326678b4
ryan o <rohouens@yahoo.fr>
2018-04-19 11:04:04 +0200

    init version

---
+++ UserManager.php
@@ -0,0 +1,24 @@
+<?php
+class UserManager extends Manager{
+    const TABLE_NAME = "user";
+
+    public function add(Raw $user){
+
+    }
+
+    public function delete(Raw $user){
+
+    }
+
+    public function get($id){
+
+    }
+
+    public function getList(){
+
+    }
+
+    public function update(Raw $user){
+        
+    }
+}
