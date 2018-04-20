a12e83f226b1c358fecaaeb5fc7f7c50cb8a1275
ryan o <rohouens@yahoo.fr>
2018-04-20 14:29:26 +0200

    parent redifined

--- User.php
+++ User.php
@@ -1,12 +1,9 @@
 <?php
-class User extends Raw{
+class User extends Track{
     protected $_pseudo;
     protected $_email;
     protected $_password;
-    protected $_key;
     protected $_extra;
-    protected $_active;
-    protected $_creation;
 
     public function getPseudo(){
         return $this->_pseudo;
@@ -20,20 +17,8 @@ class User extends Raw{
         return $this->_password;
     }
 
-    public function getKey(){
-        return $this->_key;
-    }
-
     public function getExtra(){
         return $this->_extra;
-    }
-
-    public function getActive(){
-        return $this->_active;
-    }
-
-    public function getCreation(){
-        return $this->_creation;
     }
 
     public function setPseudo($pseudo){
@@ -76,31 +61,7 @@ class User extends Raw{
         $this->_password = $password;
     }
 
-    public function setKey($key){
-        if(!preg_match("#^[a-zA-Z0-9]{32}$#", $key)){
-            trigger_error("Key must contain 32 characters", E_USER_WARNING);
-            return;
-        }
-        $this->_key = $key;
-    }
-
     public function setExtra($extra){
         $this->_extra = $extra;
-    }
-
-    public function setActive($active){
-        if(!preg_match("#^[0-9]{1}$#", $active)){
-            trigger_error("Activation code must be 1 number", E_USER_WARNING);
-            return;
-        }
-        $this->_active = (int)$active;
-    }
-
-    public function setCreation($creation){
-        if(!preg_match("#^[0-9]{1,}$#", $creation)){
-            trigger_error("Date of creation must be an integer", E_USER_WARNING);
-            return;
-        }
-        $this->_creation = (int)$creation;
     }
 }
