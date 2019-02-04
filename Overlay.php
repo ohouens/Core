2293339b40509b6b207cfc620ef943bb4d330978
ryan o <rohouens@yahoo.fr>
2019-02-04 14:24:22 +0100

    adding of manager in overlay

--- Overlay.php
+++ Overlay.php
@@ -1,15 +1,21 @@
 <?php
 class Overlay{
     protected $_raw;
+    protected $_manager;
     protected $_dataName;
 
-    public function __construct(Raw $raw, $dataName){
+    public function __construct(Raw $raw, Manager $manager, $dataName){
         $this->_raw = $raw;
+        $this->_manager = $manager;
         $this->_dataName = $dataName;
     }
 
     public function getRaw(){
         return $this->_raw;
+    }
+
+    public function getManager(){
+        return $this->_manager;
     }
 
     public function getDataName(){
